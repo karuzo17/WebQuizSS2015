8a8b59af056d8e018323b7d3a99c1a293f8acae9
TimH <haerle17@gmail.com>
2015-05-25 20:29:51 +0200

    Ajax Fixed
    Pfade angepasst

--- WebContent/catalog.js
+++ WebContent/catalog.js
@@ -4,9 +4,9 @@
 
 function init(){
 	
-	alert("ALARM");
+	
 	request= new XMLHttpRequest();
-	request.open("GET","/WebQuiz2015/catalogs", true);
+	request.open("GET","/WebQuizSS15/catalogs", true);
 	request.onreadystatechange=handler;
 	request.send();
 }
@@ -16,16 +16,15 @@ function handler(){
 	
 	switch (request.readyState) {
 	case 2:
-
+		
 		break;
 	case 4:
-
+		
 		var antwort = request.responseText;
 		
 		
 		var bla= JSON.parse(antwort);
-
-		
+			
 		var catDiv = document.getElementById("catalog");
 		var length=bla.length;
 		
@@ -55,17 +54,16 @@ function handler(){
 function catalogClicked(event){
 	var catalogClicked = event.target;
     var catalogsTMP = document.getElementsByClassName("catalogDiv");
-    alert(catalogsTMP.length);
-    alert(catalogClicked.id);
+    
     for(var j = 0; j < catalogsTMP.length; j++){
-    	alert(catalogClicked.id +" "+catalogsTMP[j].id);
+    	
         if(catalogClicked === catalogsTMP[j]){
-        	alert("grün");
+        	
             var divToChange = document.getElementById("catalog"+j);
             divToChange.style.background = "#7C907A";
         }
         else{
-        	alert("grau");
+        	
             var defaultColorDiv = document.getElementById("catalog"+j);
             defaultColorDiv.style.background = "Whitesmoke";
         }
