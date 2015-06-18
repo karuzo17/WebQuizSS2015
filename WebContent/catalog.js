463f6e3064b66d224bfe36662e6e80cae33882fc
Daniel <d.lutschinski@web.de>
2015-06-18 21:53:32 +0200

    Läuft!
    Blablu bissl am Arbeiten. Timer wird versetzt!
    Gleich gehts weiter!

--- WebContent/catalog.js
+++ WebContent/catalog.js
@@ -1,6 +1,5 @@
-/**
- * 
- */
+document.addEventListener('DOMContentLoaded', init, false);
+
 function init() {
 
 	request = new XMLHttpRequest();
@@ -96,73 +95,3 @@ function setTmpCat(catName) {
 		}
 	}
 }
-//function init(){
-//	
-//	
-//	request= new XMLHttpRequest();
-//	request.open("GET","/WebQuizSS15/catalogs", true);
-//	request.onreadystatechange=handler;
-//	request.send();
-//}
-//
-//function handler(){
-//	
-//	
-//	switch (request.readyState) {
-//	case 2:
-//		
-//		break;
-//	case 4:
-//		
-//		var antwort = request.responseText;
-//		
-//		
-//		var bla= JSON.parse(antwort);
-//			
-//		var catDiv = document.getElementById("catalog");
-//		var length=bla.length;
-//		
-//	for(var i=0;i<length;i++){
-//		
-//		var div=document.createElement("div");
-//		div.className="catalogDiv";
-//		div.id="catalog"+i;
-//		
-//		var divname = document.createTextNode(bla[i]);
-//		
-//		div.appendChild(divname);
-//		div.addEventListener("click",catalogClicked,false);
-//		catDiv.appendChild(div);
-//		
-//	}
-//
-//		break;
-//
-//	default:
-//		
-//		break;
-//	
-//	}
-//}
-////dickes Problem 
-//function catalogClicked(event){
-//	console.log(event.target.innerHTML);
-//	catalogSelected(event.target.innerHTML);
-//	var catalogClicked = event.target;
-//	
-//    var catalogsTMP = document.getElementsByClassName("catalogDiv");
-//   
-//    for(var j = 0; j < catalogsTMP.length; j++){
-//    	
-//        if(catalogClicked === catalogsTMP[j]){
-//        	
-//            var divToChange = document.getElementById("catalog"+j);
-//            divToChange.style.background = "#7C907A";
-//        }
-//        else{
-//        	
-//            var defaultColorDiv = document.getElementById("catalog"+j);
-//            defaultColorDiv.style.background = "Whitesmoke";
-//        }
-//    }
-//}
--- WebContent/login.js
+++ WebContent/login.js
@@ -6,6 +6,8 @@ var firstPlayer = false;
 var tmpCat = false;
 var animation;
 var timer;
+var question;
+var waitingForNewQuestion = false;
 
 function initLogin() { // Listener registrieren fÃ¼r Buttons
 	var login = window.document.getElementById("loginButton");
@@ -117,10 +119,10 @@ function empfange(message) {
 	}
 
 	if (json.QUESTION) {
-		gameOn();
-		tableCreate();
+
 		var question = json.QUESTION;
 		createQuestion(question);
+		waitingForNewQuestion = false;
 	}
 }
 
@@ -144,13 +146,17 @@ function startGame() {
 			"GAMESTART" : true
 		});
 		socket.send(start);
-	}else{
+	} else {
 		alert("first pick cat!");
+	}
+	if (tmpCat) {
+		gameOn();
+		tableCreate();
 	}
 }
 
 function catalogSelected(name) {
-	
+
 	tmpCat = true;
 	var cat = JSON.stringify({
 		"CATALOG" : name
@@ -166,21 +172,22 @@ function gameOn() {
 	frameDiv.replaceChild(questionSection, mainSection);
 }
 
-function createQuestion(question) {
+function createQuestion(questionFromServer) {
 
-	document.getElementById("questionText").innerHTML = question[0];
 	for (var aC = 0; aC < 4; aC++) {
-		document.getElementById("answer" + aC).innerHTML = question[aC + 1];
+		document.getElementById("answer" + aC).innerHTML = questionFromServer[aC + 1];
 	}
 	// Noch Feld/Platz finden/designen
-	timer = question[5]/1000;
-	document.getElementById("timerRow").innerHTML = "Seconds left:  " + timer;
+	timer = questionFromServer[5] / 1000;
+	question = questionFromServer[0];
+	document.getElementById("questionText").innerHTML = questionFromServer[0]
+			+ "<br /><br /><br />" + "Seconds left:  " + timer;
 	initTimer();
 }
 
 function tableCreate() {
-	var questionSection = document.getElementById("newMain"), tbl = document
-			.createElement('table');
+	var questionSection = document.getElementById("newMain");
+	tbl = document.createElement('table');
 	tbl.id = "showQuestionTable";
 
 	var aC = 0;
@@ -201,12 +208,8 @@ function tableCreate() {
 			tc.addEventListener("click", answerClicked, false);
 		}
 	}
-	
-	var timerRow = document.createElement("div");
-	timerRow.id = "timerRow";
-	
+
 	questionSection.appendChild(tbl);
-	questionSection.appendChild(timerRow);
 }
 
 function removeLoginButton() {
@@ -217,7 +220,6 @@ function removeLoginButton() {
 
 	loginForm.removeChild(btn);
 
-	
 	var textDiv = document.createElement("div");
 	textDiv.id = "text";
 	textDiv.innerHTML = "Please wait until Leader starts the game!";
@@ -227,191 +229,37 @@ function removeLoginButton() {
 function setLeaderText() {
 	console.log("setLeaderText");
 	var leaderTextDiv = document.getElementById("text");
-	leaderTextDiv.innerHTML = "Please select a catalog!" 
-		+ "<br />" + "You can start the game as soon as a second player joins!";
+	leaderTextDiv.innerHTML = "Please select a catalog!" + "<br />"
+			+ "You can start the game as soon as a second player joins!";
 }
 
-function initTimer(){
-	alert("Timer in sec: " + timer);	
+function initTimer() {
 	animation = window.setInterval("countTime();", 1000);
 }
 
-function countTime(){
+function countTime() {
 
-	if(timer == 0){
+	timer = timer - 1;
+	if (timer == 0) {
+		document.getElementById("questionText").innerHTML
+				+ "<br /><br /><br />" + "Seconds left:   0";
 		window.clearInterval(animation);
-		alert("Frage nicht beantwortet!");
-		document.getElementById("timerRow").innerHTML = "Seconds left:   0";
-	}else{
-		document.getElementById("timerRow").innerHTML = "Seconds left:  " + timer;
+	} else {
+		questionText.innerHTML = question + "<br /><br /><br />" + "Seconds left:  " + timer;
 	}
-	timer = timer -1;
 }
 
-function answerClicked(event){
+function answerClicked(event) {
 	var answerClicked = event.target;
+	var answerForServer;
 
-	for(var i = 0; i < 4; i++){
-		if(answerClicked === document.getElementById("answer"+i)){
-			alert("Answer " + i + " picked! :)");
+	if (!waitingForNewQuestion) {
+		for (var i = 0; i < 4; i++) {
+			if (answerClicked === document.getElementById("answer" + i)) {
+				alert("Clicked answer: " + i);
+				answerForServer = i;
+				waitingForNewQuestion = true;
+			}
 		}
 	}
 }
-
-
-//document.addEventListener('DOMContentLoaded', initLogin, false);
-// 
-// var socket;
-// var bereitZumSenden=false;
-// 
-// function initLogin()
-// {  // Listener registrieren fÃ¼r Buttons
-//	var login= window.document.getElementById("loginButton");   
-//	login.addEventListener("click",send,false); 
-//
-//	//var url = "ws://" + document.location.host  + "/Echo";
-//	var url = 'ws://localhost:8080/WebQuizSS15/Login';
-////	alert("url= "+url);
-//	
-//	socket=new WebSocket(url);
-//
-//	socket.onopen=sendenMoeglich;
-//	socket.onclose=Closing;
-//	socket.onerror=ErrorHandler;
-//	socket.onmessage=empfange;
-// }
-//
-// 
-//// Listener fÃ¼r den Button go
-// function send(event)
-// {  
-// 	var button = event.target;
-// 	var outmessage=window.document.getElementById("userName").value; 
-// 	var newPlayer =JSON.stringify({"NEWPLAYER":outmessage});
-// 	
-// 	var string =newPlayer;
-// 	var obj = JSON.parse(string);
-// 	
-//// 	var string = JSON.stringify(obj);
-// 
-// 	if ( bereitZumSenden == true)
-// 	    { alert("Jetzt senden");socket.send(newPlayer);}
-// 	else alert("Server noch nicht bereit zum Empfangen. Bitte nochmals versuchen");
-//	 
-//	
-// } 
-// 
-// function sendenMoeglich(){ bereitZumSenden=true; console.log("Senden ist möglich");}
-// function ErrorHandler(event){ alert("Fehler bei den Websockets "+event.data);}
-// function Closing(event){ 
-//	 alert("Websockets closing "+event.code);
-//	 
-//	 
-//	 var string = JSON.stringify({"LOGOUT":true});
-//	 socket.send(string);
-// 
-// }
-// 
-// function empfange(message)
-// { 
-//	 var text = message.data;
-////	 alert(text);
-//	 var json=JSON.parse(text);
-////	alert("json:"+json);
-////	 alert(json[0].username);
-//	 
-//	 if(json.QUESTION){
-//		 alert("Question erhalten");
-//		 console.log(json.QUESTION[0]);
-//		 console.log(json.QUESTION[1]);
-//	 }
-//	 if(json.CATALOG){
-////		 alert(json.CATALOG);
-//		 console.log("Hab katalog bekommen");
-//	 }
-//	 if(json.PLAYERLIST){
-////		 alert("playerlist"+json.Playerlist);
-////		 var i=0;
-////		 
-////		 for(i;i<json.Playerlist.length;i++){
-////			 alert("Name:"+json.Playerlist[i].username)
-////		 }
-//		
-//		 
-//	
-//	 var playerDiv = document.getElementById("players");
-////	 var frameDiv = document.getElementById("frame");
-////	 frameDiv.removeChild(scoreDiv);
-////	 var score= document.createElement("div");
-////	 score.id="score";
-//	 
-//	 while(playerDiv.hasChildNodes()){
-//		 playerDiv.removeChild(playerDiv.lastChild);
-//	 }
-//	 
-//	 for(var i=0;i<=json.PLAYERLIST.length;i++){
-//		 var div=document.createElement("div");
-//			div.className="playerDiv";
-//			div.id="player"+i;
-//			
-//			var divname = document.createTextNode(json.PLAYERLIST[i].username);
-//			
-//			div.appendChild(divname);
-//			
-//			playerDiv.appendChild(div);
-//		 
-//	 }
-//	 
-//	  }
-//	 if(json.GAMESTART){
-//		 console.log("spiel kann gestartet werden");
-//		 
-//		 createGameStartButton();
-//	 }
-//	
-////	 frameDiv.appendChild(score);
-//// 	var text = message.data;
-//// 	//alert("emfange Daten vom Server: "+text);
-//// 	// Textfeld zur Ausgabe des Echos vom Server
-//// 	var output=window.document.getElementById("ausgabetext");
-//// 	// Bereich zur Darstellung aller angemeldeten Clients
-//// 	var session=window.document.getElementById("sessionID"); 
-//// 	
-//// 	// es kommt ein JSON-Array mit der Liste aller Clients
-//// 	if (text.charAt(0) == "[")
-//// 	{       // JSON-String in ein Array wandeln
-//// 		    var clientliste = JSON.parse(text);
-//// 		    
-//// 		    output="";
-//// 		    // Liste aller Clients anzeigen
-//// 	        for ( i=0; i<clientliste.length; i++)  output = output+"client "+i+":"+clientliste[i]+"<br/>";
-//// 	            session.innerHTML=output;
-////    }
-//// 	// es kommt ein Echo 
-//// 	else 	output.value=text;
-//	
-// } 
-// 
-// function createGameStartButton(){
-//	 
-//	 var Button = document.createElement("input");
-//	 Button.type="button";
-//	 Button.value="Start Game";
-//	 Button.id="Button";
-//	 
-//	 var loginDiv=document.getElementById("loginForm");
-//	 loginDiv.appendChild(Button);
-//	 Button.addEventListener("click",startGame,false);
-//
-// }
-// 
-// function startGame(){
-//	 console.log("ALARM START ");
-//	 var start =JSON.stringify({"GAMESTART":true});
-//	 socket.send(start);
-// }
-// function catalogSelected(name){
-//	 console.log("Name"+name);
-//	 var cat =JSON.stringify({"CATALOG":name});
-//	 socket.send(cat);
-// }
