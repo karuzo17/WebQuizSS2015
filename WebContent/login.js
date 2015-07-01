b7a01a0542136c6f822c211f9bf464f89589cec2
lutschid <d.lutschinski@web.de>
2015-07-01 15:23:57 +0200

    Comments
    Zweite HÄlfte der Comments done in login.js

--- WebContent/login.js
+++ WebContent/login.js
@@ -328,10 +328,15 @@ function setAnswerBackground(correctA) {
 
 	var cols = document.getElementsByClassName("cols");
 
+	//sofern die gewählte Antwort auch die richtige Antwort war -> richtige Antwort GRÜN hinterlegen
 	if (sendAnswer === correctA) {
 		document.getElementById("answer" + correctA).style.background = "#BEF781";
+		
+	//wenn gar keine Antwort gewählt wurde -> richtige Antwort GRÜN hinterlegen
 	} else if (sendAnswer === 4) {
 		document.getElementById("answer" + correctA).style.background = "#BEF781";
+		
+	//webb die gegebene Antwort falsch war -> richtige Antwort GRÜN, falsch Antwort (sendAnswer) ROT
 	} else {
 		document.getElementById("answer" + sendAnswer).style.background = "#F78181";
 		document.getElementById("answer" + correctA).style.background = "#BEF781";
@@ -390,7 +395,7 @@ function catalogSelected(name) {
 	socket.send(cat);
 }
 
-//die "main"-Section wird von allen ChildNodes befreit, damit die Fragentabelle erstellt werden kann
+//die "main"-Section wird von allen ChildNodes befreit
 function cleanMain() {
 	var mainSection = document.getElementById("main");
 
@@ -402,23 +407,31 @@ function cleanMain() {
 //schreibt die aktuelle Frage in die Tabelle 
 function createQuestion(questionFromServer) {
 
+	//auffüllen der Tabelle mit den Antworten
 	for (var aC = 0; aC < 4; aC++) {
 		document.getElementById("answer" + aC).innerHTML = questionFromServer[aC + 1];
 		document.getElementById("answer" + aC).style.background = "Whitesmoke";
 	}
-
+	
+	//timeout wird ausgelesen
 	timer = questionFromServer[5] / 1000;
 	question = questionFromServer[0];
+	
+	//schreiben des Fragetextes in das Frage-Feld
 	document.getElementById("questionText").innerHTML = questionFromServer[0]
 			+ "<br /><br /><br />" + "Seconds left:  " + timer;
+	//timer wird gestartet
 	initTimer();
 }
 
+//erstellen der Tabelle für die Fragen
 function tableCreate() {
+	
 	var questionSection = document.getElementById("main");
 	tbl = document.createElement('table');
 	tbl.id = "showQuestionTable";
 
+	//erstellen der verschiedenen Zeilen und Spalten
 	var aC = 0;
 	for (var i = 0; i < 3; i++) {
 		var tr = tbl.insertRow();
@@ -438,24 +451,31 @@ function tableCreate() {
 			tc.addEventListener("click", answerClicked, false);
 		}
 	}
-
+	
+	//anhängen der Tabelle an die "main"-Section
 	questionSection.appendChild(tbl);
 }
 
+//der Login-Button wird immer entfernt, sobald sich ein Spieler angemeldet hat, damit keine
+//weitere Loginfunktion mehr möglich ist
 function removeLoginButton() {
+	
+	//entfernen des Buttons
 	console.log("RemoveLoginButton");
 	var btn = document.getElementById("loginButton");
 	var userName = document.getElementById("userName");
 	var loginForm = document.getElementById("loginForm");
-
 	loginForm.removeChild(btn);
-
+	
+	//hinzufügen eines Textes, der bis zum Spielstart zum Warten auffordert
 	var textDiv = document.createElement("div");
 	textDiv.id = "text";
 	textDiv.innerHTML = "Please wait until Leader starts the game!";
 	loginForm.replaceChild(textDiv, userName);
 }
 
+//der Spielleiter bekommt einen anderen Text vor dem Spielstart, der ihn auffordert einen Katalog zu wählen 
+//und das er die Möglichkeit hat bei Anmeldung eines zweiten Spielers das Spiel zu starten 
 function setLeaderText() {
 	console.log("setLeaderText");
 	var leaderTextDiv = document.getElementById("text");
@@ -463,15 +483,19 @@ function setLeaderText() {
 			+ "You can start the game as soon as a second player joins!";
 }
 
+//diese Funktion realisiert den Timer, der auf eine Animation zurückgreift
 function initTimer() {
 	animation = window.setInterval("countTime();", 1000);
 }
 
+//aktualisiert den Timer und schreibt die verbleibende Zeit unter den Fragetext
 function countTime() {
 
 	var questionText = document.getElementById("questionText");
-
 	timer = timer - 1;
+	
+	//sobald der Timer ohne Antwortwahl ausläuft, wird er gestoppt, die Antwort "4" an den Server
+	//gesendet und auf eine Anwort (Lösung der Frage gewartet)
 	if (timer == 0) {
 		questionText.innerHTML = question + "<br /><br /><br />"
 				+ "Seconds left:   0";
@@ -483,18 +507,24 @@ function countTime() {
 		console.log("sendAnswer" + sendAnswer);
 		waitingForResponse = true;
 		socket.send(response);
+		
+	//aktualisieren der Timeranzeige	
 	} else {
 		questionText.innerHTML = question + "<br /><br /><br />"
 				+ "Seconds left:  " + timer;
 	}
 }
 
+//sendet ein RESPONSE-Paket an den Server mit der gewählten Antwort 
 function answerClicked(event) {
+	
 	var answerClicked = event.target;
 	console.log("AnswerClicked");
+	
 	if (!waitingForResponse) {
-		console.log("IF___AnswerClicked");
 		for (var i = 0; i < 4; i++) {
+			
+			//rausfinden der angeklickten Antwort
 			if (answerClicked === document.getElementById("answer" + i)) {
 				sendAnswer = i;
 				window.clearInterval(animation);
@@ -503,12 +533,16 @@ function answerClicked(event) {
 				});
 				socket.send(answer);
 				console.log("Response in answerClicked versendet");
+				
+				//durch das Setzen dieser Variablen ist kein weiteres Klicken auf die Antworten
+				//mehr möglich
 				waitingForResponse = true;
 			}
 		}
 	}
 }
 
+//erstellen der Highscoreliste mithilfe einer Tabelle
 function initPlayerTable() {
 
 	var playersDiv = document.getElementById("players");
@@ -516,11 +550,13 @@ function initPlayerTable() {
 	playerTable.id = "playerTable";
 	playerTable.setAttribute("width", "90%");
 
+	//erstellen der Zeilen
 	for (var i = 0; i < 6; i++) {
 
 		var tableRow = playerTable.insertRow();
 		tableRow.id = "rows" + i;
 
+		//erstellen der Spalten/Zellen
 		for (var j = 0; j < 3; j++) {
 			var tableCell = tableRow.insertCell();
 			if (j === 0) {
@@ -540,7 +576,11 @@ function initPlayerTable() {
 	playersDiv.appendChild(playerTable);
 }
 
+//nachdem ein Spiele alle Fragen beendet hat, wartet der Server bis alle Spieler fertig sind und er
+//den Rang der Spieler versenden kann. Bis dahin wird der Spieler mit dem Text aufgefordert zu warten
 function setMainTextWait() {
+	
+	//löschen aller Elemente in der "main"-Section und hinzufügen eines Text-divs
 	cleanMain();
 	var waitText = document.createElement("div");
 	waitText.id = "texts";
@@ -548,7 +588,10 @@ function setMainTextWait() {
 	document.getElementById("main").appendChild(waitText);
 }
 
+//nachdem alle Spieler fertig sind, bekommen sie ihren Rang angezeigt
 function setMainTextRank(rank) {
+	
+	//löschen aller Elemente in der "main"-Section und hinzufügen eines Text-divs
 	cleanMain();
 	var rankText = document.createElement("div");
 	rankText.id = "texts";
@@ -556,493 +599,3 @@ function setMainTextRank(rank) {
 			+ "! Congrats and thanks for playing! :)";
 	document.getElementById("main").appendChild(rankText);
 }
-
-//document.addEventListener('DOMContentLoaded', initLogin, false);
-//window.onbeforeunload=tabClose;
-//var socket;
-//var bereitZumSenden = false;
-//var firstPlayer = false;
-//var tmpCat = false;
-//var animation;
-//var timer;
-//var question;
-//var sendAnswer;
-//var firstQuestion = true;
-//var waitingForResponse = false;
-//
-//// Dieser Text ist nur zum Pushen da
-//// Dieser Text ist nur zum Pushen da
-//// Dieser Text ist nur zum Pushen da
-//// Dieser Text ist nur zum Pushen da
-//// Dieser Text ist nur zum Pushen da
-//// Dieser Text ist nur zum Pushen da
-//
-//function initLogin() { // Listener registrieren fÃ¼r Buttons
-//
-//	startNewGame();
-//
-//	initPlayerTable();
-//
-//	var url = 'ws://localhost:8080/WebQuizSS15/Login';
-//
-//	socket = new WebSocket(url);
-//
-//	socket.onopen = sendenMoeglich;
-//	socket.onclose = Closing;
-//	socket.onerror = ErrorHandler;
-//	socket.onmessage = empfange;
-//}
-//
-//function startNewGame() {
-//
-//	var main = document.getElementById("main");
-//
-//	var loginForm = document.createElement("div");
-//	loginForm.id = "loginForm";
-//
-//	main.appendChild(loginForm);
-//
-//	var loginButton = document.createElement("input");
-//	loginButton.type = "button";
-//	loginButton.value = "Login";
-//	loginButton.id = "loginButton";
-//	loginButton.addEventListener("click", send, false);
-//
-//	var userName = document.createElement("input");
-//	userName.type = "text";
-//	userName.name = "userName";
-//	userName.id = "userName";
-//
-//	loginForm.appendChild(loginButton);
-//	loginForm.appendChild(userName);
-//
-//}
-//
-//// Listener fÃ¼r den Button go
-//function send(event) {
-//	var button = event.target;
-//	var outmessage = window.document.getElementById("userName").value;
-//	var nameUsed = false;
-//	var playerCols = document.getElementsByClassName("playerCol");
-//
-//	for (var i = 0; i < playerCols.length; i++) {
-//
-//		if (document.getElementById("playerCol" + i).innerHTML === outmessage) {
-//			alert("Name schon vergeben");
-//			nameUsed = true;
-//		}
-//	}
-//
-//	if (!nameUsed && outmessage !== "") {
-//
-//		var newPlayer = JSON.stringify({
-//			"NEWPLAYER" : outmessage
-//		});
-//		var string = newPlayer;
-//		var obj = JSON.parse(string);
-//
-//		if (bereitZumSenden == true) {
-//			socket.send(newPlayer);
-//			removeLoginButton();
-//		} else
-//			alert("Server noch nicht bereit zum Empfangen. Bitte nochmals versuchen");
-//	}
-//}
-//
-//function sendenMoeglich() {
-//	bereitZumSenden = true;
-//	console.log("Senden ist möglich");
-//}
-//
-//function ErrorHandler(event) {
-//	alert("Fehler bei den Websockets " + event.data);
-//}
-//
-//function Closing(event) {
-//
-//	
-//}
-//function tabClose(event){
-//	var string = JSON.stringify({
-//		"LOGOUT" : true
-//	});
-//	socket.send(string);
-//}
-//
-///**
-// * @param message
-// */
-///**
-// * @param message
-// */
-//function empfange(message) {
-//
-//	var text = message.data;
-//	var json = JSON.parse(text);
-////	console.log(json);
-//
-//
-//	if (json.CATALOG) {
-//
-//		console.log("Hab katalog bekommen");
-//		var catName = json.CATALOG;
-//		if (!firstPlayer) {
-//			setTmpCat(catName);
-//			tmpCat = true;
-//		}
-//	}
-//
-//	if (json.PLAYERLIST) {
-//
-//		var playerTable = document.getElementById("playerTable");
-//
-//		for (var i = 0; i < 6; i++) {
-//
-//			if (i < json.PLAYERLIST.length) {
-//				document.getElementById("playerCol" + i).innerHTML = json.PLAYERLIST[i].username;
-//				document.getElementById("scoreCol" + i).innerHTML = json.PLAYERLIST[i].score;
-//			} else {
-//				document.getElementById("playerCol" + i).innerHTML = "-";
-//				document.getElementById("scoreCol" + i).innerHTML = 0;
-//			}
-//		}
-//	}
-//
-//	if (json.GAMESTART) {
-//
-//		console.log("spiel kann gestartet werden");
-//		createGameStartButton();
-//	}
-//
-//	if (json.LEADER) {
-//
-//		firstPlayer = true;
-//		initCatListener();
-//		setLeaderText();
-//	}
-//
-//	if (json.QUESTION) {
-//
-//		if (firstQuestion) {
-//			firstQuestion = false;
-//			cleanMain();
-//			tableCreate();
-//		}
-//
-//		var question = json.QUESTION;
-//		createQuestion(question);
-//	}
-//	if(json.WAIT){
-////		alert("Warte bis alle Spieler fertig sind");
-//		setMainTextWait();
-//	}
-//
-//	if (json.ERROR) {
-//
-//		alert("Got error:");
-//	}
-//
-//	if (json.RESPONSE) {
-//		
-//		console.log("Hab die Antwort erhalten");
-//		var correctAnswer = json.RESPONSE[0];
-//		
-//		setAnswerBackground(correctAnswer);
-//		console.log("korrekte Antwort"+correctAnswer);
-//		console.log("gewähöte Antwort"+sendAnswer);
-//		setTimeout(function() {
-//			waitingForResponse = false;
-//			var question = JSON.stringify({
-//				"QUESTION" : true
-//			});
-//			socket.send(question);
-//			
-//		}, 3000);
-//	}
-//	
-//	if(json.GAMEOVER){
-//		//zu implementieren
-//		alert("GAMEOVER")
-////		defaultSettings();
-////		
-//		
-//	}
-//
-//	if(json.RANK){
-////		console.log("Du hast "+json.RANK+" erreicht");
-//		//zu implementieren
-//		setMainTextRank(json.RANK);
-//		setTimeout(3000,sendGameOver());
-//	}
-//
-//	if(json.ERROR){
-//
-//		//zu implementieren
-//		alert(json.ERROR);
-//	}
-//}
-//
-//function setMainTextWait() {
-//	cleanMain();
-//	var waitText = document.createElement("div");
-//	waitText.id = "texts";
-//	waitText.innerHTML = "GAME OVER! Please wait for the Final result!";
-//	document.getElementById("main").appendChild(waitText);
-//}
-//
-//function setMainTextRank(rank) {
-//	cleanMain();
-//	var rankText = document.createElement("div");
-//	rankText.id = "texts";
-//	rankText.innerHTML = "You finished on rank " + rank
-//			+ "! Congrats and thanks for playing! :)";
-//	document.getElementById("main").appendChild(rankText);
-//}
-//
-//function setAnswerBackground(correctA) {
-//	
-//	console.log("answer background");
-////	alert("drin!");
-//
-//	var cols = document.getElementsByClassName("cols");
-//
-//	if(sendAnswer === correctA){
-//		document.getElementById("answer" + correctA).style.background = "#BEF781";
-//	} 
-//	else if(sendAnswer === 4){
-////		alert("deine mom stinkt");
-//		document.getElementById("answer" + correctA).style.background = "#BEF781";
-//	}else{
-//		document.getElementById("answer" + sendAnswer).style.background = "#F78181";
-//		document.getElementById("answer" + correctA).style.background = "#BEF781";
-//	}
-//}
-//
-//function createGameStartButton() {
-//
-//	var Button = document.createElement("input");
-//	Button.type = "button";
-//	Button.value = "Start Game";
-//	Button.id = "Button";
-//
-//	var loginDiv = document.getElementById("loginForm");
-//	loginDiv.appendChild(Button);
-//	Button.addEventListener("click", startGame, false);
-//}
-//
-//function sendGameOver(){
-//	var gameover = JSON.stringify({
-//		"GAMEOVER" : true
-//	});
-//	socket.send(gameover);
-//}
-//function startGame() {
-//
-//	if (tmpCat) {
-//		console.log("ALARM START ");
-//		var start = JSON.stringify({
-//			"GAMESTART" : true
-//		});
-//		socket.send(start);
-//	} else {
-//		alert("first pick cat!");
-//	}
-//}
-//
-//function catalogSelected(name) {
-//
-//	tmpCat = true;
-//	var cat = JSON.stringify({
-//		"CATALOG" : name
-//	});
-//	socket.send(cat);
-//}
-//
-//function cleanMain() {
-//	var mainSection = document.getElementById("main");
-//
-//	while (mainSection.hasChildNodes()) {
-//		mainSection.removeChild(mainSection.lastChild);
-//	}
-//}
-//
-//function createQuestion(questionFromServer) {
-//
-//	for (var aC = 0; aC < 4; aC++) {
-//		document.getElementById("answer" + aC).innerHTML = questionFromServer[aC + 1];
-//		document.getElementById("answer" + aC).style.background = "Whitesmoke";
-//	}
-//
-//	timer = questionFromServer[5] / 1000;
-//	question = questionFromServer[0];
-//	document.getElementById("questionText").innerHTML = questionFromServer[0]
-//			+ "<br /><br /><br />" + "Seconds left:  " + timer;
-//	initTimer();
-//}
-//
-//function tableCreate() {
-//	var questionSection = document.getElementById("main");
-//	tbl = document.createElement('table');
-//	tbl.id = "showQuestionTable";
-//
-//	var aC = 0;
-//	for (var i = 0; i < 3; i++) {
-//		var tr = tbl.insertRow();
-//		for (var j = 0; j < 2; j++) {
-//			var tc = tr.insertCell();
-//			if (i !== 0) {
-//				tc.id = "answer" + aC;
-//				tc.className = "cols";
-//				tc.setAttribute("width", "55%");
-//				aC++;
-//			} else {
-//				if (j === 0) {
-//					tc.setAttribute("colspan", "2");
-//					tc.id = "questionText";
-//				}
-//			}
-//			tc.addEventListener("click", answerClicked, false);
-//		}
-//	}
-//
-//	questionSection.appendChild(tbl);
-//}
-//
-//function removeLoginButton() {
-//	console.log("RemoveLoginButton");
-//	var btn = document.getElementById("loginButton");
-//	var userName = document.getElementById("userName");
-//	var loginForm = document.getElementById("loginForm");
-//
-//	loginForm.removeChild(btn);
-//
-//	var textDiv = document.createElement("div");
-//	textDiv.id = "text";
-//	textDiv.innerHTML = "Please wait until Leader starts the game!";
-//	loginForm.replaceChild(textDiv, userName);
-//}
-//
-//function setLeaderText() {
-//	console.log("setLeaderText");
-//	var leaderTextDiv = document.getElementById("text");
-//	leaderTextDiv.innerHTML = "Please select a catalog!" + "<br />"
-//			+ "You can start the game as soon as a second player joins!";
-//}
-//
-//function initTimer() {
-//	animation = window.setInterval("countTime();", 1000);
-//}
-//
-//function countTime() {
-//	
-//
-//	var questionText = document.getElementById("questionText");
-//	
-//	timer = timer - 1;
-//	if (timer == 0) {
-//		questionText.innerHTML = question + "<br /><br /><br />"
-//				+ "Seconds left:   0";
-//		window.clearInterval(animation);
-//		var response = JSON.stringify({
-//			"RESPONSE" : 4
-//		});
-//		sendAnswer = 4;
-//		console.log("sendAnswer"+sendAnswer);
-//		waitingForResponse=true;
-//		socket.send(response);
-//	} else {
-//		questionText.innerHTML = question + "<br /><br /><br />"
-//				+ "Seconds left:  " + timer;
-//	}
-//}
-//
-//function answerClicked(event) {
-//	var answerClicked = event.target;
-//	console.log("AnswerClicked");
-//	if (!waitingForResponse) {
-//		console.log("IF___AnswerClicked");
-//		for (var i = 0; i < 4; i++) {
-//			if (answerClicked === document.getElementById("answer" + i)) {
-//				sendAnswer = i;
-//				window.clearInterval(animation);
-//				var answer = JSON.stringify({
-//					"RESPONSE" : sendAnswer
-//				});
-//				socket.send(answer);
-//				console.log("Response in answerClicked versendet");
-//				waitingForResponse = true;
-//			}
-//		}
-//	} 
-//}
-//function defaultSettings() {
-//	
-//	bereitZumSenden = false;
-//	firstPlayer = false;
-//	tmpCat = false;
-//	firstQuestion = true;
-//	waitingForResponse = false;
-//	
-//	for (var i = 0; i < 6; i++) {
-//		document.getElementById("playerCol" + i).innerHTML = "-";
-//		document.getElementById("scoreCol" + i).innerHTML = 0;
-//		document.getElementById("rows" + i).style.background = "Whitesmoke";
-//	}
-//
-//	var catalogsTMP = document.getElementsByClassName("catalogDiv");
-//	for (var j = 0; j < catalogsTMP.length; j++) {
-//		document.getElementById("catalog" + j).style.background = "Whitesmoke";
-//	}
-//
-//	var main = document.getElementById("main");
-//	document.getElementById("main").innerHTML = "";
-//
-//	var loginForm = document.createElement("div");
-//	loginForm.id = "loginForm";
-//
-//	main.appendChild(loginForm);
-//
-//	var loginButton = document.createElement("input");
-//	loginButton.type = "button";
-//	loginButton.value = "Login";
-//	loginButton.id = "loginButton";
-//	loginButton.addEventListener("click", send, false);
-//
-//	var userName = document.createElement("input");
-//	userName.type = "text";
-//	userName.name = "userName";
-//	userName.id = "userName";
-//
-//	loginForm.appendChild(userName);
-//	loginForm.appendChild(loginButton);
-//}
-//
-//function initPlayerTable() {
-//
-//	var playersDiv = document.getElementById("players");
-//	var playerTable = document.createElement("table");
-//	playerTable.id = "playerTable";
-//
-//	for (var i = 0; i < 6; i++) {
-//
-//		var tableRow = playerTable.insertRow();
-//		for (var j = 0; j < 3; j++) {
-//
-//			var tableCell = tableRow.insertCell();
-//			if (j === 0) {
-//				tableCell.innerHTML = i + 1 + ".";
-//				tableCell.setAttribute("width", "20%");
-//			} else if (j === 1) {
-//				tableCell.id = "playerCol" + i;
-//				tableCell.innerHTML = "-";
-//				tableCell.className = "playerCol";
-//				tableCell.setAttribute("width", "55%");
-//			} else {
-//				tableCell.id = "scoreCol" + i;
-//				tableCell.innerHTML = "-";
-//				tableCell.setAttribute("width", "25%");
-//			}
-//		}
-//	}
-//	playersDiv.appendChild(playerTable);
-//}
